d479b24cd5d852512e051c18c7bca1302f1ed088
mahmoudazizorignal <mahmoudazizorignal@gmail.com>
2022-03-28 23:14:34 +0200

    Main Program

---
+++ Avg_Of_Three_Nums.cpp
@@ -0,0 +1,17 @@
+/*
+    Author: Mahmoud Mamdouh
+    Purpose: The Program Calculates the Average of Three Integers Numbers
+
+
+*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[3];
+    cout << "ENTER 3 INTEGERS TO PRINT THE AVERAGE: ";
+    for(int i = 0; i < 3; i++)
+        cin >> arr[i];
+    cout << (arr[0] + arr[1] + arr[2]) / 3 << endl;
+    return 0;
+}
